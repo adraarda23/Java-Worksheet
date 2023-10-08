bedb71c76437c9a914472e5861057b94ba0ad76e
adraarda23 <ardaaydinkilinc@gmail.com>
2023-10-08 12:57:48 +0300

    Exercise01 completed and some notes were taken

---
+++ Worksheet/src/Day01/Exercise01.java
@@ -0,0 +1,20 @@
+package Day01;
+
+public class Exercise01{
+	public static int main(String[] args){
+		return 0;
+		
+	}
+}
+
+
+
+
+/*
+ Note1: There can be a only one public class and its name must be same as file name
+ Note2: main method cannot return any value // it should be void (?? i guess ??)
+ Note3: {String ...} syntax same as String[] in compile phase
+ 
+ 
+ 
+ */
