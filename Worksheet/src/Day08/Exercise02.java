76dd04cecc0ea5429b310029f4737c76f4c195bb
adraarda23 <ardaaydinkilinc@gmail.com>
2023-10-21 00:48:23 +0300

    java.util.ArrayList

---
+++ Worksheet/src/Day08/Exercise02.java
@@ -0,0 +1,17 @@
+package Day08;
+import java.util.ArrayList;
+
+public class Exercise02 {
+	//java.util.ArrayList 
+    public static void main(String[] args) {
+        ArrayList<Integer> liste = new ArrayList<>();
+
+        liste.add(5);
+        liste.add(10);
+        liste.add(15);
+
+        System.out.println(liste);
+    }
+}
+
+//Note1: With this ArrayList can printed directly and it can have same methods to manipulate it
