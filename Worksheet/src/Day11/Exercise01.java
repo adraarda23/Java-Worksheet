b6b029fe273a362d4ecda2ae724f6a4ef4c59391
adraarda23 <ardaaydinkilinc@gmail.com>
2023-10-24 22:34:04 +0300

    Constructor Rules in Java

---
+++ Worksheet/src/Day11/Exercise01.java
@@ -0,0 +1,10 @@
+package Day11;
+public class Exercise01{
+	public static void main(String[] args) {
+		
+	}
+}
+
+//Note1: Every Object have Constructor method (Even if its not declared compiler adds that)
+//Note2: Constructors must have same name with object
+//Note3: Constructors can be overloaded
