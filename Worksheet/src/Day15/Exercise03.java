4acea08509cba3447a18fbb00ed56a8a0065f759
adraarda23 <ardaaydinkilinc@gmail.com>
2023-10-29 01:57:46 +0300

    package and file address compile codes

---
+++ Worksheet/src/Day15/Exercise03.java
@@ -0,0 +1,15 @@
+package Day15;
+//EASY ADDRESS
+
+//javac -d ./bin x.java
+//javac -d ./bin *.java -> compiles all of them (better solution)
+
+
+public class Exercise03 {
+
+	public static void main(String[] args) {
+		
+
+	}
+
+}
