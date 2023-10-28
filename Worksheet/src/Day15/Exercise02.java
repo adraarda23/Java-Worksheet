65a4d7b7b6861ad1d967d0d164f65c182b4b1f77
adraarda23 <ardaaydinkilinc@gmail.com>
2023-10-28 18:36:01 +0300

    import and static import

---
+++ Worksheet/src/Day15/Exercise02.java
@@ -0,0 +1,16 @@
+package Day15;
+//import static java.lang.Math.*;
+import static java.lang.Math.PI;
+public class Exercise02 {
+
+	public static void main(String[] args) {
+		double test = PI;
+		System.out.println(test);
+
+	}
+
+}
+//Note1: all packages can import with { * }
+//Note2: * only imports one layer lower  it doesn't call upper domains
+//Note3: Same named files must be called with full path as a reason avoiding conflicts
+//Note4: static variables can imported by import static
