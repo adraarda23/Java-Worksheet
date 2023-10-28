ec9b4101fbb0571ce32ceb5256fdfc7c9294201a
adraarda23 <ardaaydinkilinc@gmail.com>
2023-10-28 11:44:59 +0300

    Package Tricks

---
+++ Worksheet/src/Day15/Exercise01.java
@@ -0,0 +1,13 @@
+package Day15; //package must be the first written code !!
+
+public class Exercise01 {
+	//Package
+
+	public static void main(String[] args) {
+		
+	}
+
+}
+//Note1: In Java, a source file can contain only one package declaration.
+//Note2: Package is not necessary
+//Note3: namespaces avoid name conflicts example -> com.facebook.business
