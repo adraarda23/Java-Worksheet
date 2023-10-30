328e88edbe270b48fa92c41c8d716ebacb8724d5
adraarda23 <ardaaydinkilinc@gmail.com>
2023-10-31 00:05:39 +0300

    JavaBean

---
+++ Worksheet/src/Day17/JavaBean.java
@@ -0,0 +1,27 @@
+package Day17;
+//Java Bean Example
+public class JavaBean {
+
+	
+		private String ad;
+	    private int yas;  //private
+
+	    public JavaBean() {
+	    }
+
+	    public String getAd() {
+	        return ad;
+	    }
+
+	    public void setAd(String ad) {
+	        this.ad = ad;
+	    }
+
+	    public int getYas() {
+	        return yas;
+	    }
+
+	    public void setYas(int yas) {
+	        this.yas = yas;
+	    }
+}
