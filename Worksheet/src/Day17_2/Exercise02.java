9d8e69995850699ccac8fe34a03c6f042262f966
adraarda23 <ardaaydinkilinc@gmail.com>
2023-10-31 11:56:10 +0300

    Inheritance basics

---
+++ Worksheet/src/Day17_2/Exercise02.java
@@ -0,0 +1,16 @@
+package Day17_2;
+//Inheritance
+public class Exercise02 {
+
+	public static void main(String[] args) {
+//		public class arda extends mehmet{
+//			
+//		}
+
+	}
+
+}
+
+//Note 1: Constructors and private members cannot be inherited by a child class.
+//Note 2: Public, protected, and classes within the same package can be inherited.
+// **Class inheritance variables and accesses must be **
