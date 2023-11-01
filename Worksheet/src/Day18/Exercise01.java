47e12b059769ba67c58021e4c8e4391df7547a4f
adraarda23 <ardaaydinkilinc@gmail.com>
2023-11-01 16:10:55 +0300

    Super constructor

---
+++ Worksheet/src/Day18/Exercise01.java
@@ -0,0 +1,14 @@
+package Day18;
+
+public class Exercise01 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
+//Note1: after extends keyword there can be a only one class name ,multiple inheritance with extends keyword is not allowed
+//Note2: Constructors cannot inherited it has to call with super(), super() calls parent class constructors
+//Note3: super() have to be in class if it isn't declared java creates for user in compiling time
+//Note4: super() must be the first code works
