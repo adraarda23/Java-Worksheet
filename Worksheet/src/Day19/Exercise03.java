80a02d3164e9c04c073a73a322da535860829161
adraarda23 <ardaaydinkilinc@gmail.com>
2023-11-04 04:21:39 +0300

    Overriding

---
+++ Worksheet/src/Day19/Exercise03.java
@@ -0,0 +1,14 @@
+package Day19;
+//Over Riding
+public class Exercise03 {
+	//@Override
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
+
+//Note1: @Override protects from defects
+//Note2: Overloading returns different things with different inputs  but
+//Overriding manipulates inherited functions functionality
