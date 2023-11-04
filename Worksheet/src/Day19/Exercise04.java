e37c99be03c053ba95bc9bd24090de75d11ef83a
adraarda23 <ardaaydinkilinc@gmail.com>
2023-11-04 04:22:10 +0300

    final classes and methods

---
+++ Worksheet/src/Day19/Exercise04.java
@@ -0,0 +1,20 @@
+package Day19;
+
+public class Exercise04 {
+	public static void main(String[] args) {
+
+
+	}
+
+}
+final class arda{
+	
+}
+//FINAL CLASS
+//Note1: final classes cannot inherited by other class 
+//Note2: class cannot have protected variables if it has final keyword
+//because those variables cannot have chance to inherited (nonsense)
+
+//FINAL METHOD
+//Note3: If a method of an object is declared as final, that method cannot be overridden.
+//Note4: If a method of a class is declared as final, that method cannot be hidden.
