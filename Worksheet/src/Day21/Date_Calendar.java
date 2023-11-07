d30f83580b34756b320c44b15151eb8a5311b147
adraarda23 <ardaaydinkilinc@gmail.com>
2023-11-07 16:54:11 +0300

    java.time.* and java.util.Calendar/Date libraries

---
+++ Worksheet/src/Day21/Date_Calendar.java
@@ -0,0 +1,17 @@
+package Day21;
+import java.util.Date;
+import java.util.Calendar;
+import java.time.*; //inspect this library for extended time manipulation
+public class Date_Calendar {
+	//OLD API
+	public static void main(String[] args) {
+		Date date=new Date();
+		
+	}
+
+}
+
+
+//Note1: Date calculates value of seconds passed after 1 January 1970
+//Note2: Date hold values as a long type
+//Nıte3: **in a web site UTC time zone represent servers local time not users ** 
